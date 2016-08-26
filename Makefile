11305eac1261cb44a25d43c95e1f7e30331ebab0
livefr0g <iamlivefr0g@gmail.com>
2016-08-25 23:50:37 -0400

    source files added

---
+++ Makefile
@@ -0,0 +1,13 @@
+CFLAGS=-Wall -g
+
+all: logfind #tests
+
+#logfind:
+
+tests:
+	sh tests.sh
+
+clean:
+	rm logfind
+	rm -f *.o
+	rm -rf *.dSYM
---
+++ tests.sh
@@ -0,0 +1,7 @@
+./logfind .logfind h Makefile
+echo " "
+./logfind .logfind Makefile h
+echo " "
+./logfind .logfind h Makefile c
+echo " "
+./logfind .logfind h Makefile asdf c
